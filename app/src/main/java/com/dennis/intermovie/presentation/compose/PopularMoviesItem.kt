adf37eb9aba73de7ad1afef0659b87e51ae9e1d6
Dennis Chandrian <Dennischandrian@gmail.com>
2023-11-22 21:11:59 +0700

    Add preview composable to popular movie list

--- app/src/main/java/com/dennis/intermovie/presentation/compose/PopularMoviesItem.kt
+++ app/src/main/java/com/dennis/intermovie/presentation/compose/PopularMoviesItem.kt
@@ -3,14 +3,18 @@ package com.dennis.intermovie.presentation.compose
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dennis.intermovie.domain.model.PopularMoviesItem
+import com.dennis.intermovie.presentation.ui.theme.InterMovieTheme
 
 @Composable
 fun PopularMoviesItem(
@@ -48,6 +52,20 @@ fun PopularMoviesItem(
                     overflow = TextOverflow.Ellipsis
                 )
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InterMovieTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            PopularMoviesListScreen()
         }
     }
 }
